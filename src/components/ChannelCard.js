db58e10445f071e6c1ac5f6caa27597e9a5da4a6
Mizzoo <87053514+Mizzuu@users.noreply.github.com>
2022-11-20 17:56:45 -0800

    pass marginTop props

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
     return (
         <Box
             sx={{
@@ -16,6 +16,7 @@ const ChannelCard = ({ channelDetail }) => {
                 width: { xs: "356px", md: "320px" },
                 height: "326px",
                 margin: "auto",
+                marginTop,
             }}
         >
             <Link to={`/channel/${channelDetail?.id?.channelId}`}>
@@ -48,11 +49,17 @@ const ChannelCard = ({ channelDetail }) => {
                             sx={{ fontSize: 14, color: "gray", ml: "5px" }}
                         />
                     </Typography>
-                    {channelDetail?.statistics?.subscribeCount && (
-                        <Typography>
+                    {channelDetail?.statistics?.subscriberCount && (
+                        <Typography
+                            sx={{
+                                fontSize: "15px",
+                                fontWeight: 500,
+                                color: "gray",
+                            }}
+                        >
                             {parseInt(
-                                channelDetail?.statistics?.subscribeCount
-                            ).toLocaleString()}{" "}
+                                channelDetail?.statistics?.subscriberCount
+                            ).toLocaleString("en-US")}{" "}
                             Subscribers
                         </Typography>
                     )}
